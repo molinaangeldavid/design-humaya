938aff9cb2255fe283813d22f496be512f623d1b
david <molina.angeldavid@gmail.com>
2022-09-11 23:22:47 -0300

    add the hamburguer functionality and work it

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function changeDisplay() {
+    var x = document.getElementById("hidden-b-mobile");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
